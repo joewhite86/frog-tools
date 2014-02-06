a210704312a519998b6129de69bbd0aa359dd63f
Jochen Weis <jochen@whitefrog.de>
2014-02-06 15:54:25 +0100

    added MapUtils

---
+++ src/main/java/de/whitefrog/utils/MapUtils.java
@@ -0,0 +1,39 @@
+package de.whitefrog.utils;
+
+import java.util.*;
+
+public class MapUtils {
+  public static <K extends Comparable<? super K>, V> Map<K, V> sortByKey( Map<K, V> map ) {
+    List<Map.Entry<K, V>> list = new LinkedList<>( map.entrySet() );
+
+    Collections.sort( list, new Comparator<Map.Entry<K, V>>() {
+      public int compare( Map.Entry<K, V> o1, Map.Entry<K, V> o2 ) {
+        return (o1.getKey()).compareTo( o2.getKey() );
+      }
+    });
+
+    Map<K, V> result = new LinkedHashMap<>();
+    for (Map.Entry<K, V> entry : list) {
+      result.put( entry.getKey(), entry.getValue() );
+    }
+
+    return result;
+  }
+
+  public static <K extends Comparable<? super K>, V> Map<K, V> sortByValue( Map<K, V> map ) {
+    List<Map.Entry<K, V>> list = new LinkedList<>( map.entrySet() );
+
+    Collections.sort( list, new Comparator<Map.Entry<K, V>>() {
+      public int compare( Map.Entry<K, V> o1, Map.Entry<K, V> o2 ) {
+        return (o1.getKey()).compareTo( o2.getKey() );
+      }
+    });
+
+    Map<K, V> result = new LinkedHashMap<>();
+    for (Map.Entry<K, V> entry : list) {
+      result.put( entry.getKey(), entry.getValue() );
+    }
+
+    return result;
+  }
+}
