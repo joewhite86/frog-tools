e7894bd9eb829f30d6412236bd9540cd552b754c
Jochen Weis <jochen@whitefrog.de>
2014-03-13 12:29:56 +0100

    added javascript helper

---
+++ src/main/java/de/whitefrog/neo4j/helper/QueryResult.java
@@ -0,0 +1,10 @@
+package de.whitefrog.neo4j.helper;
+
+public class QueryResult {
+  public String[] columnNames;
+  public Object[][] result;
+  public QueryResult(String[] columnNames, Object[][] result) {
+    this.columnNames = columnNames.clone();
+    this.result = result.clone();
+  }
+}
