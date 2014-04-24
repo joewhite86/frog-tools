c40140bd3700676977449a5e34cdfcb6b07c0034
Jochen Weis <jochen@whitefrog.de>
2014-04-24 18:10:17 +0200

    added singular/plural formatting

--- src/main/java/de/whitefrog/utils/TimeUtils.java
+++ src/main/java/de/whitefrog/utils/TimeUtils.java
@@ -1,10 +1,8 @@
 package de.whitefrog.utils;
 
+import java.text.MessageFormat;
 import java.util.concurrent.TimeUnit;
 
-/**
- * Created by jochen on 14.04.14.
- */
 public class TimeUtils {
   public static String formatInterval(long i) {
     final long days = TimeUnit.MILLISECONDS.toDays(i);
@@ -14,19 +12,19 @@ public class TimeUtils {
     final long ms = TimeUnit.MILLISECONDS.toMillis(i - TimeUnit.HOURS.toMillis(hr) - TimeUnit.MINUTES.toMillis(min) - TimeUnit.SECONDS.toMillis(sec));
 
     if(days > 0) {
-      return String.format("%d days %d hours", days, hr);
+      return MessageFormat.format("{0} {0,choice,0#days|1#day|1<days} {1} {1,choice,0#hours|1#hour|1<hours}", days, hr);
     }
     else if(hr > 0) {
-      return String.format("%d hours %d minutes", hr, min);
+      return MessageFormat.format("{0} {0,choice,0#hours|1#hour|1<hours} {1} {1,choice,0#minutes|1#minute|1<minutes}", hr, min);
     }
     else if(min > 0) {
-      return String.format("%d minutes %d seconds", min, sec);
+      return MessageFormat.format("{0} {0,choice,0#minutes|1#minute|1<minutes} {1} {1,choice,0#seconds|1#second|1<seconds}", min, sec);
     }
     else if(sec > 0) {
-      return String.format("%d seconds %d ms", sec, ms);
+      return MessageFormat.format("{0} {0,choice,0#seconds|1#second|1<seconds} {1}ms", days, hr);
     }
     else {
-      return String.format("%dms", ms);
+      return MessageFormat.format("{0}ms", ms);
     }
   }
 }
