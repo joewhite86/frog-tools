08d632c246582315326a250a3c86f23e115e5ace
Jochen Weis <jochen@whitefrog.de>
2014-04-24 18:16:45 +0200

    fixed time calc

--- src/main/java/de/whitefrog/utils/TimeUtils.java
+++ src/main/java/de/whitefrog/utils/TimeUtils.java
@@ -6,10 +6,10 @@ import java.util.concurrent.TimeUnit;
 public class TimeUtils {
   public static String formatInterval(long i) {
     final long days = TimeUnit.MILLISECONDS.toDays(i);
-    final long hr = TimeUnit.MILLISECONDS.toHours(i);
-    final long min = TimeUnit.MILLISECONDS.toMinutes(i - TimeUnit.HOURS.toMillis(hr));
-    final long sec = TimeUnit.MILLISECONDS.toSeconds(i - TimeUnit.HOURS.toMillis(hr) - TimeUnit.MINUTES.toMillis(min));
-    final long ms = TimeUnit.MILLISECONDS.toMillis(i - TimeUnit.HOURS.toMillis(hr) - TimeUnit.MINUTES.toMillis(min) - TimeUnit.SECONDS.toMillis(sec));
+    final long hr = TimeUnit.MILLISECONDS.toHours(i - TimeUnit.DAYS.toMillis(days));
+    final long min = TimeUnit.MILLISECONDS.toMinutes(i - TimeUnit.DAYS.toMillis(days) - TimeUnit.HOURS.toMillis(hr));
+    final long sec = TimeUnit.MILLISECONDS.toSeconds(i - TimeUnit.DAYS.toMillis(days) - TimeUnit.HOURS.toMillis(hr) - TimeUnit.MINUTES.toMillis(min));
+    final long ms = TimeUnit.MILLISECONDS.toMillis(i - TimeUnit.DAYS.toMillis(days) - TimeUnit.HOURS.toMillis(hr) - TimeUnit.MINUTES.toMillis(min) - TimeUnit.SECONDS.toMillis(sec));
 
     if(days > 0) {
       return MessageFormat.format("{0} {0,choice,0#days|1#day|1<days} {1} {1,choice,0#hours|1#hour|1<hours}", days, hr);
